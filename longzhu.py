e517ccf359ab3e13db9bff24bdf05e17e00e36c8
maguoliang <maguoliang@tv189.com>
2020-08-11 13:48:32 +0800

    longzhu support get rid

--- longzhu.py
+++ longzhu.py
@@ -2,7 +2,7 @@
 
 import requests
 import re
-
+from lxml import etree
 
 def get_real_url(rid):
     try:
@@ -14,8 +14,31 @@ def get_real_url(rid):
         real_url = '直播间不存在或未开播'
     return real_url
 
+def get_longzhu_room_id():
+    rids = []
+    
+    url = 'http://longzhu.com/channels/all'
+    try:
+        html = requests.get(url).text
+        selector = etree.HTML(html)
+        urls = selector.xpath("//a[@class='livecard']/@href")
+        for l in urls:
+            # //star.longzhu.com/2440233?from=challcontent
+            match = re.search(r'/(\d+)\?', l)
+            if match:
+                rid = match.group(1)
+                rids.append(rid)
+        return rids
+    except Exception as e:
+        print(expr(e))
+    return None
+        
+rids = get_longzhu_room_id()
+for rid in rids:
+    real_url = get_real_url(rid)
+    print('%s: %s' % (rid, real_url))
 
-rid = input('请输入龙珠直播房间号：\n')
-real_url = get_real_url(rid)
-print('该直播间源地址为：')
-print(real_url)
+#rid = input('请输入龙珠直播房间号：\n')
+#real_url = get_real_url(rid)
+#print('该直播间源地址为：')
+#print(real_url)
